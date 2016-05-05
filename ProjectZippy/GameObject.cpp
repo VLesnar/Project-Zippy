3cd6d108a5109abb27996cea4b1922c3bec07950
Alexander Bronner <acbronn@gmail.com>
2016-05-05 19:41:01 -0400

    More base game object class.

--- ProjectZippy/GameObject.cpp
+++ ProjectZippy/GameObject.cpp
@@ -1,8 +1,9 @@
 #include "GameObject.h"
 
-GameObject::GameObject(string objectName, string modelName, string colID)
+GameObject::GameObject(string objectName, string modelName, string colID, matrix4 intransform)
 {
-	transform = IDENTITY_M4;
+	name = objectName;
+	transform = intransform;
 	m_pMeshMngr->LoadModel(modelName, objectName);
 	BOMngr->SetBO(m_pMeshMngr->GetVertexList("objectName"), objectName, colID);
 }
@@ -12,12 +13,25 @@ GameObject::~GameObject()
 
 }
 
-void GameObject::setModelMatrix(matrix4 transform)
+void GameObject::setModelMatrix(matrix4 intransform)
 {
-
+	transform = intransform;
+	establishModelMatrix();
 }
 
-void GameObject::push(vector3 dir)
+void GameObject::translate(vector3 dir)
 {
+	transform = glm::translate(transform, dir);
+	establishModelMatrix();
+}
 
+void GameObject::establishModelMatrix()
+{
+	m_pMeshMngr->SetModelMatrix(transform, name);
+	BOMngr->SetModelMatrix(name, transform);
+}
+
+void GameObject::render()
+{
+	m_pMeshMngr->AddInstanceToRenderList(name);
 }
--- ProjectZippy/GameObject.h
+++ ProjectZippy/GameObject.h
@@ -5,13 +5,16 @@
 class GameObject
 {
 protected:
+	string name;
 	matrix4 transform;
 	MyBoundingObjectClass* BO = nullptr;
 	MyBoundingObjectManager* BOMngr = MyBoundingObjectManager::GetInstance();
 	MeshManagerSingleton* m_pMeshMngr = MeshManagerSingleton::GetInstance();
 public:
-	GameObject(string objectName, string modelName, string colID);
+	GameObject(string objectName, string modelName, string colID, matrix4 intransform);
 	~GameObject();
-	void setModelMatrix(matrix4 transform);
-	void push(vector3 dir);
+	void setModelMatrix(matrix4 intransform);
+	void translate(vector3 dir);
+	void establishModelMatrix();
+	void render();
 };
